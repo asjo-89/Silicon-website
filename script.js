f7f51f1393867bb0da5edf00f47416bd177cac8f
Åsa Johansson <asa.j.1989@gmail.com>
2024-09-19 12:06:45 +0200

    grund för accordion lagd och js skapat för stängningsfunktion

---
+++ script.js
@@ -0,0 +1,13 @@
+const radioButtons = document.querySelectorAll('input[name="radio"]');
+        let isChecked = null;
+
+        radioButtons.forEach(radio => {
+            radio.addEventListener('click', function() {
+                if (this === isChecked) {
+                    this.checked = false;
+                    isChecked = null;
+                } else {
+                    isChecked = this;
+                }
+            });
+        });
